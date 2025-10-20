fed08ea1aec588c04141dea1541f1c9dd2795f17
Avigail <29akravets@gannacademy.org>
2025-10-20 12:05:01 -0400

    HEYYYYY

--- src/App.java
+++ src/App.java
@@ -6,15 +6,26 @@ public class App extends PApplet{
     }
 
     public void setup(){
+        background(238, 199, 54);
+        fill(255); 
+        noStroke();
+        rectMode(CENTER); 
+        rect(width / 2, height / 2, 800, 600); 
+        stroke(0);
+        strokeWeight(5);
         
     }
 
     public void settings(){
+         size(1000, 800); 
         
     }
 
     public void draw(){
-       
+        
 
+    }
+    public void mouseDragged() {
+        line(pmouseX, pmouseY, mouseX, mouseY);
     }
 }
